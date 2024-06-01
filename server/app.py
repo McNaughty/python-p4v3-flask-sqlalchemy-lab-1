1bd6f70b7883baef5e3d51ac49ec4782fcd1412b
Winnie <wimacoj@gmail.com>
2024-06-01 10:30:34 +0300

    Completed assignment.

--- server/app.py
+++ server/app.py
@@ -21,6 +21,30 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquake(id):
+    quake = Earthquake.query.filter(Earthquake.id == id).first()
+    if quake:
+        body = quake.to_dict()
+        status = 200
+    else:
+        body = {'message': f'Earthquake {id} not found'}
+        status = 404
+
+    return make_response(body, status)
+
+
+@app.route('/earthquakes/magnitude/<float:maginitude>')
+def magntd(magnitude):
+    quakemagntd = []
+
+    for equake in Earthquake.query.filter(Earthquake.magnitude >= id).all():
+        quakemagntd.append(equake.to_dict())
+    body = {'count': len(quakemagntd),
+            'quakes':quakemagntd
+            }
+    return make_response(body, 200)
+
 
 
 if __name__ == '__main__':
